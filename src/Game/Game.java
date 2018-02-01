f2b83e4e6caa2e12e0a2edf1438e3fcdd185e2f9
Botho <loic.botho@gmail.com>
2018-02-01 11:59:04 -0500

    User choose between human or computer

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -2,14 +2,13 @@ package Game;
 
 import Game.Player.Computer;
 import Game.Player.Human;
+import Game.Player.Player;
 
 public class Game {
-    private Game.Player.Player player1, player2;
-    private boolean player2Human;
+    private Player player1, player2;
 
-    public Game() {
-        player2Human = true;
+    public Game(Boolean player2Human) {
         player1 = new Human();
-        if (player2Human) player2 = new Human(); else player2=new Computer();
+        if (player2Human) player2 = new Human(); else player2 = new Computer();
     }
 }
--- src/Game/Main.java
+++ src/Game/Main.java
@@ -1,9 +1,28 @@
 package Game;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Grid grid = new Grid();
-        grid.displayGrid();
+        Boolean player2human = player2Choice();
+        Game game = new Game(player2human);
+    }
+
+    private static Boolean player2Choice () {
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        Boolean player2Human = false;
+        System.out.println("You want to play against a human player or a computer ? (human/computer)");
+        String str = reader.nextLine();
+        reader.close();
+        if (str.equals("human")) {
+            player2Human = true;
+        } else if (str.equals("computer")) {
+            player2Human = false;
+        } else {
+            System.out.println("Error, you have to write 'computer' or 'human'. Try again.");
+            player2Choice();
+        }
+        return player2Human;
     }
 }
